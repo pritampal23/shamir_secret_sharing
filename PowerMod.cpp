642d65efd92af592fcc9bcd7b212a890193c6934
pritampal23 <iampritampal23@gmail.com>
2023-11-10 23:29:14 +0530

    for calculating a^e mod n

---
+++ PowerMod.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<NTL/ZZ.h>
+
+using namespace std;
+using namespace NTL;
+
+ZZ powermod(const ZZ& a, const ZZ& e, const ZZ& n){
+    if(e == 0)
+        return ZZ(1);
+    ZZ res;
+    long k = NumBits(e);
+
+    res = 1;
+    long i = k-1;
+    while(i >= 0){
+        //res = (res * res) % n;
+        SqrMod(res,res,n);
+        if(bit(e,  i) == 1){
+            //res = (res * a) % n;
+            res = MulMod(res,a,n);
+        }
+        i--;
+    }
+    if(e < 0)
+        return InvMod(res, n);
+    else
+        return res;
+}
