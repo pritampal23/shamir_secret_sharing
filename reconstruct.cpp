1873a282115b3e727300160b66f5c3bcdf84509b
pritampal23 <iampritampal23@gmail.com>
2023-11-11 01:03:49 +0530

    reconstructing the secret

---
+++ reconstruct.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stdint.h>
+#include<NTL/ZZ.h>
+#include<NTL/vector.h>
+
+
+using namespace std;
+using namespace NTL;
+
+ZZ reconstruct(const uint64_t *party, Vec<ZZ>& c, uint64_t t, ZZ p){
+    ZZ secret;
+    secret = 0;
+    uint64_t i=0, j;
+    ZZ delta, x, y;
+    delta = 1;
+
+    while(i < t+1){
+        j = 0;
+        x = 1;
+        y = 1;
+        while(j < t +1){
+            if(j != i){
+                x = (x * party[j]) % p;
+                if(party[i] > party[j])
+                     y = (y * (party[i] - party[j])) % p;
+                else
+                     y = (y * (p + party[i] - party[j])) % p;
+            }
+            j++;
+        }
+        if((t & 1) == 1)
+            x = (p - x);
+        delta = (x * InvMod(y, p)) % p;
+        delta = (delta * c[i]) % p;
+        secret = (secret + delta) % p;
+        i++;
+    }
+    return secret;
+}
